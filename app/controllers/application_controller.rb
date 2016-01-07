bb166331bd52b65d06cade48d3bec64cbb5020f7
Aaron Sumner <asumner@mac.com>
2016-01-07 03:26:28 -0600

    Require admin to manage items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def require_admin
+    unless current_user && current_user.admin?
+      redirect_to root_url, notice: "You must be an admin to do that."
+    end
+  end
 end
