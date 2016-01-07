e75368803589e258578721ef941f20f9f729a803
Aaron Sumner <asumner@mac.com>
2016-01-07 03:15:18 -0600

    Add simple admin role on Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107082219) do
+ActiveRecord::Schema.define(version: 20160107091130) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160107082219) do
     t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128, null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
