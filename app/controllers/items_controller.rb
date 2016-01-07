591908afe47d505f042c5dae901c893c01d23638
Aaron Sumner <asumner@mac.com>
2016-01-07 02:31:07 -0600

    Password protect item management

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :require_login, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   # GET /items
