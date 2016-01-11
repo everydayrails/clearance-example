656259587e6f209f976c81491e3c49690389728e
Aaron Sumner <asumner@mac.com>
2016-01-11 03:46:42 -0600

    Add routing constraints based on login status

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   resources :items
-  root "items#index"
+
+  constraints Clearance::Constraints::SignedIn.new { |user| user.admin? } do
+    root to: "admin#index", as: "admin_root"
+  end
+
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: "items#index", as: "signed_in_root"
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: "marketing#index"
+  end
 end
